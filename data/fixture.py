3af52ab8cc2d7d7f03e80f9c050b69fb27914800
Bhuvan Kapoor <bhuvan.kapoor@harness.io>
2022-01-22 22:18:03 +0530

    update port again

--- data/fixture.py
+++ data/fixture.py
@@ -1,4 +1,4 @@
-baseUrl = "http://127.0.0.1:5000"
+baseUrl = "http://0.0.0.0:105"
 studentDetailsFile = "/data/studentDetails.json"
 multipleStudentDetailsFile = "/data/multipleStudentDetails.json"
 jobSearchFile = "/data/jobSearch.json"
